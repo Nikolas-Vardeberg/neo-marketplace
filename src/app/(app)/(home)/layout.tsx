bdc7c563c919996dd4d09d264ad3a80eddfb3339
niklusrook <nikolas.vardeberg1@gmail.com>
2025-05-01 19:50:01 +0200

    feat: search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,41 @@
+import { getPayload } from "payload";
 import Footer from "./footer";
 import { Navbar } from "./navbar";
+import { SearchFilter } from "./search-filters";
+import configPromise from "@payload-config"
+import { Category } from "@/payload-types";
 
 interface Props {
     children: React.ReactNode;
 }
 
-export default function Layout({ children }: Props) {
+export default async function Layout({ children }: Props) {
+    const payload = await getPayload({
+        config: configPromise,
+    })
+
+    const data = await payload.find({
+        collection: "categories",
+        depth: 1,
+        where: {
+            parent: {
+                exists: false,
+            },
+        }
+    })
+
+    const formattedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            ...(doc as Category),
+            subcategories: undefined,
+        }))
+    }))
+
     return(
         <div className="flex flex-col min-h-screen">
             <Navbar />
+            <SearchFilter data={formattedData} />
             <div className="flex-1 bg-[#f4f4f0]">
                 {children}
             </div>
---
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Category } from "@/payload-types";
+import { isolateObjectProperty } from "payload";
+import { useRef, useState } from "react";
+import { useDropdownPosition } from "./use-dropdown-position";
+import { SubcategoryMenu } from "./subcategory-menu";
+
+interface props {
+    category: Category;
+    isActive?: boolean;
+    isNavigationHovered?: boolean;
+}
+
+export const CategoryDropdown = ({ category, isActive, isNavigationHovered }: props) => {
+    const [isOpen, setisOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const { getDropdownPosition } = useDropdownPosition(dropdownRef);
+
+    const onMouseEnter = () => {
+        if (category.subcategories) {
+            setisOpen(true);
+        }
+    }
+
+    const onMouseLeave = () => setisOpen(false);
+
+    const dropdownPosition = getDropdownPosition();
+    
+    return(
+        <div
+            ref={dropdownRef}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            className="relative"
+        >
+            <div className="relative">
+                <Button
+                    variant="elevated"
+                    className={cn("h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+                        isActive && !isNavigationHovered && "bg-white border-primary"
+                    )}
+                    >
+                    {category.name}
+                </Button>
+                {category.subcategories && category.subcategories.length > 0 && (
+                    <div className={cn("absolute opacity-0 -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+                        isOpen && "opacity-100"
+                    )}>
+                    </div>
+                )}
+            </div>
+
+            <SubcategoryMenu
+                category={category}
+                isOpen={isOpen}
+                position={dropdownPosition}
+            />
+        </div>
+    )
+}
