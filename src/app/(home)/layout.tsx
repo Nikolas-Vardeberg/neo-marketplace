d7bb57a12eb4c837ad60c19620fbd9b5c444a812
niklusrook <nikolas.vardeberg1@gmail.com>
2025-05-01 17:13:48 +0200

    feat: navbar

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./navbar";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+    return(
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            {children}
+        </div>
+    )
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="elevated">
+        Button
+      </Button>
+      <Input placeholder="Hei" />
+      <Progress value={50} />
+      <Textarea placeholder="textarea" />
+    </div>
+  );
+}
