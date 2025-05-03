501cd10f10e6b711f40ae506966ee79951808d7f
niklusrook <nikolas.vardeberg1@gmail.com>
2025-05-04 01:46:40 +0200

    feat: filtering products

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,3 +1,4 @@
+import { ProductFilters } from "@/modules/products/ui/components/product-filter";
 import {ProductList, ProductListSkelton} from "@/modules/products/ui/components/product-list";
 import { caller, getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
@@ -20,9 +21,19 @@ export default async function Page({ params }: Props) {
 
     return(
         <HydrationBoundary state={dehydrate(queryClient)}>
-            <Suspense fallback={<ProductListSkelton />}>
-                <ProductList category={category} />
-            </Suspense>
+            <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
+                <div className="grid gird-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
+                    <div className="lg:col-span-2 xl:col-span-2">
+                        <ProductFilters />
+                    </div>
+                    <div className="lg:col-span-4 xl:col-span-6">
+                        <Suspense fallback={<ProductListSkelton />}>
+                            <ProductList category={category} />
+                        </Suspense>
+                    </div>
+                </div>
+            </div>
+           
         </HydrationBoundary>
     )
 }
